c7dff12d8155366df64ccef1eb9ab203f9e3ac2b
Alex T <alexandre.tamavond@gmail.com>
2022-11-11 21:02:26 -0500

    module2-2

---
+++ ssg/parsers.py
@@ -0,0 +1,10 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+    
+    extensions: List[str] = []
+    
+    def valid_extension(self, extension):
+        return extension in self.extensions
